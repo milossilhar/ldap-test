d9de9eebf8bdf6ccc69897bcc88d820728cf5056
milossilhar <migmig095@gmail.com>
2022-12-31 11:09:21 +0100

    updated cicd script

--- cicd.sh
+++ cicd.sh
@@ -3,5 +3,7 @@
 need_pull=$(git fetch --dry-run 2>&1)
 
 if [ -n "$need_pull" ] ; then
-  echo_title "PULLING LATEST SOURCE CODE"
+  echo "PULLING LATEST SOURCE CODE"
+else
+  echo "NOT PULLING"
 fi
